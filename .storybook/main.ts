8d6379f75835e8b774c42fac9cea8bd6d6984c56
Laurie Reynolds <lauriesreynolds@gmail.com>
2026-02-19 17:44:30 -0800

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+    "storybook-addon-docusaurus"
+  ],
+  "framework": "@storybook/react-webpack5"
+};
+export default config;
