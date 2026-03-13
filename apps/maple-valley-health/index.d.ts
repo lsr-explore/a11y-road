1b20c9672ab1be14cb77db5ef34d49628c85392a
Laurie Reynolds <lauriesreynolds@gmail.com>
2026-03-13 12:34:24 -0700

    clean up auto generated files

--- apps/maple-valley-health/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.svg' {
-  import type { FC, SVGProps } from 'react';
-  const content: string;
-  export const ReactComponent: FC<SVGProps<SVGSVGElement>>;
-  export default content;
-}
